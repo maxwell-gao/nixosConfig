875bda6f84b4a80c6fdd23d66e85acbbcf711597
maxwell-gao <maxwell-gao314@outlook.com>
2026-03-29 17:47:52 +0800

    feat: add backupFileExtension to home-manager configuration

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.max = import ./hosts/x1c13/home.nix;
           }
@@ -52,6 +53,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.nixos = import ./hosts/wsl/home.nix;
           }
